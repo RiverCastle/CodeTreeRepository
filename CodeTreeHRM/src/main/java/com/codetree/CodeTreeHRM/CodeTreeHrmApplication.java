52637c22c5cc1fa37d48a8e2a8109a6ec3136b66
RiverCastle <rkdtjd12@gmail.com>
2026-04-10 19:27:14 +0900

    [CodeTreeHRM] first commit

---
+++ CodeTreeHRM/src/main/java/com/codetree/CodeTreeHRM/CodeTreeHrmApplication.java
@@ -0,0 +1,13 @@
+package com.codetree.CodeTreeHRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeTreeHrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeTreeHrmApplication.class, args);
+	}
+
+}
---
+++ CodeTreeHRM/src/test/java/com/codetree/CodeTreeHRM/CodeTreeHrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codetree.CodeTreeHRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeTreeHrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
